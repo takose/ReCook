c0132800a16e37d92766d5484b42af1b5d042eb5
tako <rina.m.takase@gmail.com>
2018-07-22 23:40:48 +0900

    Use tslint

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,8 @@
 const { environment } = require('@rails/webpacker')
 const typescript =  require('./loaders/typescript')
+const tslint =  require('./loaders/tslint')
+
 
 environment.loaders.append('typescript', typescript)
+environment.loaders.append('tslint', tslint)
 module.exports = environment
