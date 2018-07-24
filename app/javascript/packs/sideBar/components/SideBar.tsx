6d90c76f0bd5f8956bdb371ea558164036b9305a
tako <rina.m.takase@gmail.com>
2018-07-24 15:03:57 +0900

    Implement SideBar

--- app/javascript/packs/sideBar/components/SideBar.tsx
+++ app/javascript/packs/sideBar/components/SideBar.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+import {
+  wrapper as Wrapper,
+  item as Item,
+  icon as Icon,
+  navLink,
+} from '../styles/Sidebar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faPlay, faSearch, faCog } from '@fortawesome/free-solid-svg-icons';
 
 export interface Props {
 }
@@ -7,11 +16,36 @@ export interface Props {
 class SideBar extends React.Component<Props, object> {
   render() {
     return (
-      <div className="sidebar">
-        <Link to="/explore">Explore</Link>
-        <Link to="/editor">Editor</Link>
-        <Link to="/player">Player</Link>
-      </div>
+      <Wrapper>
+        <Item>
+          <NavLink to="/explore" style={{ opacity: 0.5 }} activeStyle={{ opacity: 1 }}>
+          <Icon>
+            <FontAwesomeIcon icon={faSearch} />
+          </Icon>
+          </NavLink>
+        </Item>
+        <Item>
+          <NavLink to="/editor" style={{ opacity: 0.5 }} activeStyle={{ opacity: 1 }}>
+            <Icon>
+              <FontAwesomeIcon icon={faEdit} />
+            </Icon>
+          </NavLink>
+        </Item>
+        <Item>
+          <NavLink to="/player" style={{ opacity: 0.5 }} activeStyle={{ opacity: 1 }}>
+            <Icon>
+              <FontAwesomeIcon icon={faPlay} />
+            </Icon>
+          </NavLink>
+        </Item>
+        <Item>
+          <NavLink to="/settings" style={{ opacity: 0.5 }} activeStyle={{ opacity: 1 }}>
+            <Icon>
+              <FontAwesomeIcon icon={faCog} />
+            </Icon>
+          </NavLink>
+        </Item>
+      </Wrapper>
     );
   }
 }
