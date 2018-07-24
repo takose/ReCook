8b4cb9521931cb7e830970466b48979959cc28c5
tako <rina.m.takase@gmail.com>
2018-07-24 21:11:03 +0900

    Add interface

--- app/javascript/packs/sideBar/components/SideBar.tsx
+++ app/javascript/packs/sideBar/components/SideBar.tsx
@@ -13,6 +13,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faPlay, faSearch, faCog } from '@fortawesome/free-solid-svg-icons';
 
 export interface Props {
+  nickname?: string;
+  image_url?: string;
 }
 
 class SideBar extends React.Component<Props, object> {
