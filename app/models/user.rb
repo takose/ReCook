46a0398033e43235e555f9d47edc1bb67efe9822
tako <rina.m.takase@gmail.com>
2018-07-24 18:31:45 +0900

    Add columns for credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,15 @@
 #
 # Table name: users
 #
-#  id         :bigint(8)        not null, primary key
-#  provider   :string
-#  uid        :string
-#  nickname   :string
-#  image_url  :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :bigint(8)        not null, primary key
+#  provider        :string
+#  uid             :string
+#  nickname        :string
+#  image_url       :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  consumer_token  :string
+#  consumer_secret :string
 #
 
 class User < ApplicationRecord
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_24_082212) do
+ActiveRecord::Schema.define(version: 2018_07_24_084903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2018_07_24_082212) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "consumer_token"
+    t.string "consumer_secret"
   end
 
 end
