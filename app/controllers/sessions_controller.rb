1859a69b40d98c7f6447e78702f400c4a7d0669c
tako <rina.m.takase@gmail.com>
2018-07-24 17:51:48 +0900

    Implement sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,9 @@
 class SessionsController < ApplicationController
+  def create
+    auth_hash = request.env['omniauth.auth']
+    user = User.find_or_create_by_auth_hash(auth_hash)
+
+    session[:user_id] = user.id
+    redirect_to root_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint(8)        not null, primary key
+#  provider   :string
+#  uid        :string
+#  nickname   :string
+#  image_url  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
+  def self.find_or_create_by_auth_hash(auth_hash)
+    return if auth_hash.provider != "twitter"
+
+    uid = auth_hash[:uid]
+    nickname = auth_hash[:info][:nickname]
+    image_url = auth_hash[:info][:image]
+
+    User.find_or_create_by(provider: provider, uid: uid) do |user|
+      user.nickname = nickname
+      user.image_url = image_url
+    end
+  end
 end
