5add646ef276a9338d144ad7b563453a5bdb4359
tako <rina.m.takase@gmail.com>
2018-07-24 17:28:59 +0900

    Setup omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 
   get 'recipes/*path', to: 'recipes#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/auth/twitter/callback', to: 'sessions#create'
 end
