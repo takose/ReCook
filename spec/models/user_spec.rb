ec7582d65b331d02d96ce30e86524f2f19ab8f81
tako <rina.m.takase@gmail.com>
2018-07-24 18:32:09 +0900

    Annotate

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  provider        :string
+#  uid             :string
+#  nickname        :string
+#  image_url       :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  consumer_token  :string
+#  consumer_secret :string
+#
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
