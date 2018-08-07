94aadeb3c7f0ca683b650d58b6773bb28e21be8c
tako <rina.m.takase@gmail.com>
2018-08-07 18:45:35 +0900

    Add session action andconstants

--- app/javascript/actions/index.ts
+++ app/javascript/actions/index.ts
@@ -14,3 +14,17 @@ export function switchPiece(pieceId: string): SwitchPiece {
     type: constants.SWITCH_PIECE,
   };
 }
+
+export interface Session {
+  type: constants.LOG_IN;
+  nickname: string;
+  imageUrl: string;
+}
+
+export function login(nickname: string, imageUrl: string): Session {
+  return {
+    nickname,
+    imageUrl,
+    type: constants.LOG_IN,
+  };
+}
--- app/javascript/constants/index.ts
+++ app/javascript/constants/index.ts
@@ -1,2 +1,5 @@
 export const SWITCH_PIECE = 'SWITCH_PIECE';
 export type SWITCH_PIECE = typeof SWITCH_PIECE;
+
+export const LOG_IN = 'LOG_IN';
+export type LOG_IN = typeof LOG_IN;
