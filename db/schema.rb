ee3ccbc471b7442091847dccdaaaac4cf90f8fb1
tako <rina.m.takase@gmail.com>
2018-08-24 14:20:01 +0900

    Add pieces model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_24_084903) do
+ActiveRecord::Schema.define(version: 2018_08_24_051827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pieces", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider"
