48a7636bd39d97bebb900886cc875ff7db3272a7
tako <rina.m.takase@gmail.com>
2018-08-26 20:19:06 +0900

    Introduce redux-saga

---
+++ app/javascript/sagas/index.ts
@@ -0,0 +1,13 @@
+import { call, put, takeEvery, takeLatest, select } from 'redux-saga/effects'
+import { CREATE_STEP, CREATE_FF_STEP, FF_ID } from '../constants';
+
+function* createFFStep() {
+  const getFFStepId = state => state.ff[state.ff.length - 1].id;
+  const ffStepId = yield select(getFFStepId);
+  yield put({ type: CREATE_STEP, pieceId: FF_ID, stepId: ffStepId });
+}
+function* reCookSaga() {
+  yield takeEvery(CREATE_FF_STEP, createFFStep);
+}
+
+export default reCookSaga;
