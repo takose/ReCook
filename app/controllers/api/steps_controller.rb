97a020ed9acddbbe9449132c5845a43848ffdb23
tako <rina.m.takase@gmail.com>
2018-09-04 13:29:51 +0900

    Modify api response

--- app/controllers/api/steps_controller.rb
+++ app/controllers/api/steps_controller.rb
@@ -9,9 +9,9 @@ class Api::StepsController < ApplicationController
       # recipe = Recipe.create(user_id: current_user.id)
       recipe = Recipe.create(user_id: User.first.id) # TODO
     end
-    res = JSON.parse(request.body.read)
-    step = Step.new({piece_id: res['pieceId'], content: res['content']})
+    body = JSON.parse(request.body.read)
+    step = Step.new({piece_id: body['pieceId'], content: body['content']})
     step.update_attribute(:recipe_id, recipe.id)
-    render json: step.content
+    render json: step
   end
 end
