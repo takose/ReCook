6af0594712d66f3ed3b77e10047cc6ba7a9f6c97
tako <rina.m.takase@gmail.com>
2018-09-05 19:25:42 +0900

    Bugfix

--- app/controllers/api/steps_controller.rb
+++ app/controllers/api/steps_controller.rb
@@ -3,13 +3,13 @@ class Api::StepsController < ApplicationController
 
   def create
     recipe = ''
-    if params[:recipeId]
-      recipe = Recipe.find(params[:recipeId])
+    body = JSON.parse(request.body.read)
+    if body['recipeId']
+      recipe = Recipe.find(body['recipeId'])
     else
       # recipe = Recipe.create(user_id: current_user.id)
       recipe = Recipe.create(user_id: User.first.id) # TODO
     end
-    body = JSON.parse(request.body.read)
     step = Step.new({piece_id: body['pieceId'], content: body['content']})
     step.update_attribute(:recipe_id, recipe.id)
     render json: step
