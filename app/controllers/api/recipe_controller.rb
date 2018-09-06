6b2cf17091c4e89f3de240914c03d8c209c7a511
tako <rina.m.takase@gmail.com>
2018-09-06 15:06:17 +0900

    Implement recipe api

--- app/controllers/api/recipe_controller.rb
+++ app/controllers/api/recipe_controller.rb
@@ -1,2 +1,16 @@
 class Api::RecipeController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def create
+    recipe = ''
+    body = JSON.parse(request.body.read)
+    if body['recipeId']
+      recipe = Recipe.find(body['recipeId'])
+    else
+      # recipe = Recipe.create(user_id: current_user.id)
+      recipe = Recipe.create(user_id: User.first.id) # TODO
+    end
+    recipe.update_attribute(:title, body['title'])
+    render json: recipe 
+  end
 end
