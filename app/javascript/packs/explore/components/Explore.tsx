be1a727cd39037e20956a764f404f9c8c849816a
tako <rina.m.takase@gmail.com>
2018-09-07 13:53:40 +0900

    Implement explore view

--- app/javascript/packs/explore/components/Explore.tsx
+++ app/javascript/packs/explore/components/Explore.tsx
@@ -1,13 +1,41 @@
 import * as React from 'react';
+import { RecipeState } from '../../../types';
+import {
+  main as Main,
+  recipeItem as RecipeItem,
+  recipeList as RecipeList,
+} from '../styles/Explore';
 
 export interface Props {
+  recipes: RecipeState[];
+  setRecipes(recipes: RecipeState[]): void;
 }
 
 class Explore extends React.Component<Props, object> {
+  componentWillMount() {
+    fetch('/api/recipes')
+      .then(res => res.json())
+      .then((res) => {
+        this.props.setRecipes(res);
+      });
+  }
   render() {
+    const recipeList = this.props.recipes.map((recipe) => {
+      return (
+        <RecipeItem>
+          {recipe.title}
+        </RecipeItem>
+      );
+    });
+
     return (
-      <div className="explore">
-        here is explore
+      <div>
+        <Main>
+          here is explore
+          <RecipeList>
+            {recipeList}
+          </RecipeList>
+        </Main>
       </div>
     );
   }
--- app/javascript/packs/explore/containers/Explore.tsx
+++ app/javascript/packs/explore/containers/Explore.tsx
@@ -1,4 +1,20 @@
 import { connect } from 'react-redux';
+import { StoreState, RecipeState } from '../../../types';
+import * as actions from '../../../actions';
 import Explore from '../components/Explore';
 
-export default connect()(Explore);
+export function mapStateToProps(state: StoreState) {
+  return {
+    recipes: state.recipes,
+  };
+}
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    setRecipes: (recipes: RecipeState[]) => {
+      dispatch(actions.setRecipes(recipes));
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Explore);
