ae4315b3d4653ba68ea64cca63b8e54567f6309a
tako <rina.m.takase@gmail.com>
2018-09-12 18:12:38 +0900

    Implement LinkedList getter

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -17,4 +17,15 @@ class Recipe < ApplicationRecord
 
   belongs_to :origin_recipe, class_name: 'Recipe', foreign_key: 'origin_id', optional: true
   has_many :forked_recipes, class_name: 'Recipe'
+  def sorted_steps
+    result = []
+    before_id = nil
+    loop do
+      step = steps.find_by(next_id: before_id)
+      break unless step
+      result.unshift(step)
+      before_id = step.id
+    end
+    result
+  end
 end
