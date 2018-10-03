33883d60e647f09c5e8ddc27b893e3fb05898f6f
tako <rina.m.takase@gmail.com>
2018-10-03 12:11:12 +0900

    Configure for deploy

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
+const tslint =  require('./loaders/tslint')
 const environment = require('./environment')
 
+environment.loaders.append('tslint', tslint)
 module.exports = environment.toWebpackConfig()
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,6 @@
 const { environment } = require('@rails/webpacker')
 const typescript =  require('./loaders/typescript')
-const tslint =  require('./loaders/tslint')
 
 
 environment.loaders.append('typescript', typescript)
-environment.loaders.append('tslint', tslint)
 module.exports = environment
