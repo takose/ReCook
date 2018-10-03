613f83954f1ac1a7665112ad55164ba0f2a4bbb8
tako <rina.m.takase@gmail.com>
2018-10-03 12:59:59 +0900

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Piece.all.count <= 0
+  Piece.create([
+    {name: 'FF'},
+    {name: 'Text'},
+    {name: 'Taste'}
+  ])
+end
