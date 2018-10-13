3a238644f91e47f501e64461d1018c832a4f076d
tako <rina.m.takase@gmail.com>
2018-10-13 14:15:02 +0900

    Resolve warning

--- app/javascript/packs/pieces/Text/components/TextStep.tsx
+++ app/javascript/packs/pieces/Text/components/TextStep.tsx
@@ -11,7 +11,7 @@ export interface Props {
 class TextStep extends React.Component<Props, object> {
   render() {
     const lines = this.props.step.body.split('\n').map((line) => {
-      return <TextLine>{line}</TextLine>;
+      return <TextLine key={line}>{line}</TextLine>;
     });
     return (
       <div>{lines}</div>
