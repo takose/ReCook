0a56c6ac3c89c6d1c86462e5994976c303012fb8
tako <rina.m.takase@gmail.com>
2018-12-17 17:58:34 +0900

    Add Option view

---
+++ app/javascript/packs/editor/components/Option.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import * as actions from '../../../actions';
+import { connect } from 'react-redux';
+import {
+  optionWrapper as OptionWrapper,
+  optionButton as OptionButton,
+} from '../styles/Editor';
+
+export interface Props {
+  updateEditOption(direction): void;
+}
+
+class Option extends React.Component<Props, object> {
+  render() {
+    return (
+      <OptionWrapper>
+        <OptionButton onClick={() => this.props.updateEditOption('before')}>ステップを前に追加</OptionButton>
+        <OptionButton onClick={() => this.props.updateEditOption('after')}>ステップを後に追加</OptionButton>
+      </OptionWrapper>
+    );
+  }
+}
+
+export function mapStateToProps(state: object) {
+  return {
+  };
+}
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    updateEditOption: (direction) => {
+      dispatch(actions.updateEditOption(direction));
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Option);
