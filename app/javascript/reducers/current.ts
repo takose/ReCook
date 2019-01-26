ca0a6d2221bedcf1b73e8f64251ef1bf76daf856
tako <rina.m.takase@gmail.com>
2019-01-26 15:55:17 +0900

    Update reducer

--- app/javascript/reducers/current.ts
+++ app/javascript/reducers/current.ts
@@ -2,21 +2,15 @@ import { CurrentState } from '../types';
 import {
   SWITCH_PIECE,
   SWITCH_STEP,
-  RESET_EDIT_RECIPE,
-  RESET_PLAY_RECIPE,
-  SET_EDIT_RECIPE,
-  SET_PLAY_RECIPE,
+  RESET_RECIPE,
+  SET_RECIPE,
   DELETE_STEP_SUCCEEDED,
-  UPDATE_EDIT_OPTION,
 } from '../constants';
 
 const initialState = {
   pieceId: 1,
   stepId: null,
-  playRecipe: { id: null, title: '', steps: [], desc: '' },
-  editRecipe: {
-    id: null, title: '', steps: [], desc: '', option: { direction: null, stepId: null },
-  },
+  recipe: { id: null, title: '', steps: [], desc: '' },
 };
 
 export const current =
@@ -26,64 +20,30 @@ export const current =
         return {
           ...state,
           stepId: action.id,
-          editRecipe: {
-            ...state.editRecipe,
-            option: {
-              ...state.editRecipe.option,
-              stepId: null,
-              direction: null,
-            },
-          },
         };
       case SWITCH_PIECE:
         return {
           ...state,
           pieceId: action.pieceId,
         };
-      case SET_PLAY_RECIPE:
+      case SET_RECIPE:
         return {
           ...state,
-          playRecipe: {
+          recipe: {
             ...action.recipe,
           },
         };
-      case SET_EDIT_RECIPE:
+      case RESET_RECIPE:
         return {
           ...state,
-          editRecipe: {
-            ...state.editRecipe,
-            ...action.recipe,
-          },
-        };
-      case UPDATE_EDIT_OPTION:
-        return {
-          ...state,
-          stepId: null,
-          editRecipe: {
-            ...state.editRecipe,
-            option: {
-              ...state.editRecipe.option,
-              direction: action.direction,
-              stepId: state.stepId,
-            },
-          },
-        };
-      case RESET_EDIT_RECIPE:
-        return {
-          ...state,
-          editRecipe: initialState.editRecipe,
-        };
-      case RESET_PLAY_RECIPE:
-        return {
-          ...state,
-          playRecipe: initialState.playRecipe,
+          recipe: initialState.recipe,
         };
       case DELETE_STEP_SUCCEEDED:
-        const newSteps = state.editRecipe.steps.filter(step => step.id !== action.id);
+        const newSteps = state.recipe.steps.filter(step => step.id !== action.id);
         return {
           ...state,
-          editRecipe: {
-            ...state.editRecipe,
+          recipe: {
+            ...state.recipe,
             steps: newSteps,
           },
         };
